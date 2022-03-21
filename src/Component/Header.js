101d3fc6407a2fafaf4763826fba1d4900060f86
Abdennour Hachemi <nouri89@hotmail.com>
2022-03-21 22:10:00 +0000

    adding ux

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <div className="header-wraper">
         <div className="main-info">
             <h1>Full Stack developer</h1>
-            <Typed className="typed-text" strings={["UX-UI","Web design","Front-end","Back-end","Database","Testing"]}
+            <Typed className="typed-text" strings={["UX","Web design","Front-end","Back-end","Database","Testing"]}
             typeSpeed={80}
             backSpeed={80}
             loop/>
