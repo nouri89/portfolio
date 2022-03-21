478eb87cef680416b00f647b98dceacdf7276bb9
Abdennour Hachemi <nouri89@hotmail.com>
2022-03-21 20:59:38 +0000

    sending code to github

---
+++ src/Component/Header.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Typed from "react-typed"
+
+const Header = () => {
+  return (
+
+    <div className="header-wraper">
+        <div className="main-info">
+            <h1>Full Stack developer</h1>
+            <Typed className="typed-text" strings={["UX-UI","Web design","Front-end","Back-end","Database","Testing"]}
+            typeSpeed={80}
+            backSpeed={80}
+            loop/>
+            <a href="#" className="btn-main-offer">Contact me</a>
+
+
+        </div>
+       
+    
+      
+    </div>
+    
+  )
+}
+
+export default Header
