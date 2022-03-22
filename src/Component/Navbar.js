d9737975990f8a15df498f36491fddff072536c3
Abdennour Hachemi <nouri89@hotmail.com>
2022-03-22 14:38:57 +0000

    update photo

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from "./abdennourPhoto.jpg"
+import logo from "./Pictures/nouriPhoto.jpg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
