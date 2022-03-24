07a469da9fb8d067e4d96816ee4f83e0eb8ea8d4
Abdennour Hachemi <nouri89@hotmail.com>
2022-03-24 16:48:59 +0000

    add routers

---
+++ src/Component/Partical.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import Particles from "react-tsparticles";
+
+function Partical() {
+  return (
+    <Particles
+    
+      id="tsparticles"
+    
+      options={{
+        background: {
+          color: {
+            value: "white",
+          },
+        },
+        fpsLimit: 90,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 200,
+              duration: 8,
+              opacity: 0.8,
+              size: 90,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+            
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 60,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }}
+
+     
+     />
+  )
+}
+
+export default Partical
